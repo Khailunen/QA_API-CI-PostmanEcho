06bb4825811e88487dc709ed827828882bad1413
Karina Khaltunen <khaltunen@bk.ru>
2024-12-08 20:21:44 +0300

    add gradle.yml

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -2,6 +2,7 @@
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 
 public class PostmanEchoTest {
@@ -18,7 +19,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("$.[*].id");
+                .body("data", equalTo("some data"));
 
     }
 }
